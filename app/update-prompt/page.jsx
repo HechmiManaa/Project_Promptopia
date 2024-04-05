fb2f9c55ec2564323ffb739de4156c566dcb2faa
SWIPPO <hechmimanaa2@gmail.com>
2024-04-05 19:38:54 +0100

    fix typo

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect, useState, Suspense } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 
 import Form from "@components/Form";
@@ -53,13 +53,15 @@ const UpdatePrompt = () => {
   };
 
   return (
-    <Form
-      type="Edit"
-      post={post}
-      setPost={setPost}
-      submitting={submitting}
-      handleSubmit={updatePrompt}
-    />
+    <Suspense>
+      <Form
+        type="Edit"
+        post={post}
+        setPost={setPost}
+        submitting={submitting}
+        handleSubmit={updatePrompt}
+      />
+    </Suspense>
   );
 };
 
--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,6 @@
 const nextConfig = {
     experimental: {
       serverComponentsExternalPackages: ["mongoose"],
-      missingSuspenseWithCSRBailout: false,
     },
     images: {
       remotePatterns: [
